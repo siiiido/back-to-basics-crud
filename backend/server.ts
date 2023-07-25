9b6bc7ab89268e9bcc4c9eb78876740be63f1971
siiiido <discegaudere24.7@gmail.com>
2023-07-25 22:21:32 +0900

    refactor: remove app file

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,16 @@
 import 'dotenv/config';
-import app from './app';
 import mongoose from 'mongoose';
 import connectDB from './config/connectDB';
+import express from 'express';
 
+const app = express();
 const port: number = parseInt((process.env.PORT || 5001) as string, 10);
 
 connectDB();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 mongoose.connection.once('open', () => {
   app.listen(port, () => {
