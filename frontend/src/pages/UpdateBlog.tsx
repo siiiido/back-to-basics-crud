b8cc68f5534356d8bf8a259775048a6676ca33c3
siiiido <discegaudere24.7@gmail.com>
2023-08-08 14:29:06 +0900

    feat: add styling update blog page

--- frontend/src/pages/UpdateBlog.tsx
+++ frontend/src/pages/UpdateBlog.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { BlogTitle, BlogContent } from '../components/UI/Blog';
+import { FormButton } from '../components/UI/Button';
 
 const UpdateBlog = () => {
-  return <div>UpdateBlog</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
+      <div className="bg-white p-10 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-bold mb-4">Blog</h1>
+        <form className="space-y-6">
+          <BlogTitle />
+          <BlogContent />
+          <div className="flex justify-center">
+            <FormButton />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default UpdateBlog;
