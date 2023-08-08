dbeb6187c7441850fa48a3d1217c3bdf4467b773
siiiido <discegaudere24.7@gmail.com>
2023-08-08 14:08:44 +0900

    feat: add styling 404 page

--- frontend/src/pages/Error.tsx
+++ frontend/src/pages/Error.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
-
-const Error = () => {
-  return <div>Error</div>;
-};
+const Error = () => (
+  <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
+    <h1 className="text-6xl font-bold">404</h1>
+    <p className="text-2xl">Page Not Found</p>
+    <p className="text-lg">
+      Oops! The page you are looking for does not exist.
+    </p>
+  </div>
+);
 
 export default Error;
