5bec55bc1f7214099d48c7e823d99660a13cdd34
siiiido <discegaudere24.7@gmail.com>
2023-08-14 16:43:11 +0900

    feat: connect Blog Lists in frontend

--- frontend/src/pages/BlogLists.tsx
+++ frontend/src/pages/BlogLists.tsx
@@ -1,149 +1,43 @@
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-const BlogLists = () => {
-  const blogTitles = [
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-    {
-      title:
-        'During theseventeenth century, there were dozens of Franciscans in New Mexico, who kepttheir hands off the Indians so far as is known, but there were hundreds ofsettlers and soldiers who were unrestrained.',
-      id: '123',
-    },
-  ];
+
+interface Blog {
+  id: string;
+  title: string;
+  date: string;
+  content: string;
+}
+
+const BlogLists: React.FC = () => {
+  const [blogs, setBlogs] = useState<Blog[]>([]);
+
+  useEffect(() => {
+    const getBlogLists = async () => {
+      try {
+        const response = await fetch('http://localhost:5001/blog', {
+          method: 'GET',
+        });
+
+        if (response.ok) {
+          const responseData = await response.json();
+          setBlogs(responseData.reverse());
+        } else {
+          console.error('Failed to get data');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getBlogLists();
+  }, []);
 
   return (
     <div className="flex justify-center items-center h-screen bg-slate-300">
       <div className="bg-white p-10 rounded-lg shadow-lg w-1/2 h-5/6 overflow-y-scroll">
         <h1 className="text-3xl font-bold mb-8 text-center">Blog Titles</h1>
         <ul className="space-y-4">
-          {blogTitles.map((blogData, index) => (
+          {blogs.map((blogData, index) => (
             <Link
               key={index}
               to={`/specific-blog/${blogData.id}`}
