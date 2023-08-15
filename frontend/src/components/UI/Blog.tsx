e39fc6ef7ad4b4a39b9530b5e9f62dc828c2cbbb
siiiido <discegaudere24.7@gmail.com>
2023-08-15 14:25:05 +0900

    feat: add readOnly form

--- frontend/src/components/UI/Blog.tsx
+++ frontend/src/components/UI/Blog.tsx
@@ -20,6 +20,28 @@ const BlogInputTitle = forwardRef<InputRef>((props, ref) => {
   );
 });
 
+interface BlogInputTitleProps {
+  value: string;
+}
+
+const BlogTitleRead = ({ value }: BlogInputTitleProps) => {
+  return (
+    <div className="w-96 mt-7">
+      <label htmlFor="title" className="block text-gray-800 font-bold mb-2">
+        Title
+      </label>
+      <input
+        type="text"
+        id="title"
+        name="title"
+        className="w-full border border-gray-300 p-2 rounded-lg"
+        value={value}
+        readOnly
+      />
+    </div>
+  );
+};
+
 type TextAreaRef = HTMLTextAreaElement;
 
 const BlogInputContent = forwardRef<TextAreaRef>((props, ref) => {
@@ -40,6 +62,28 @@ const BlogInputContent = forwardRef<TextAreaRef>((props, ref) => {
   );
 });
 
+interface BlogContentReadProps {
+  value: string;
+}
+
+const BlogContentRead = ({ value }: BlogContentReadProps) => {
+  return (
+    <div>
+      <label htmlFor="content" className="block text-gray-800 font-bold mb-2">
+        Content
+      </label>
+      <textarea
+        id="content"
+        name="content"
+        rows="5"
+        className="w-full border border-gray-300 p-2 rounded-lg"
+        value={value}
+        readOnly
+      ></textarea>
+    </div>
+  );
+};
+
 interface BlogTitleProps {
   title: string;
 }
@@ -48,4 +92,10 @@ const BlogTitle = ({ title }: BlogTitleProps) => (
   <h1 className="text-3xl font-bold mb-4">{title}</h1>
 );
 
-export { BlogInputTitle, BlogInputContent, BlogTitle };
+export {
+  BlogInputTitle,
+  BlogInputContent,
+  BlogTitle,
+  BlogTitleRead,
+  BlogContentRead,
+};
--- frontend/src/pages/SpecificBlog.tsx
+++ frontend/src/pages/SpecificBlog.tsx
@@ -1,18 +1,56 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 import {
-  BlogInputContent,
-  BlogInputTitle,
+  BlogContentRead,
+  BlogTitleRead,
   BlogTitle,
 } from '../components/UI/Blog';
 import { FormButton } from '../components/UI/Button';
 
+interface Blog {
+  _id: string;
+  title: string;
+  date: string;
+  content: string;
+}
+
 const SpecificBlog = () => {
+  const [specificBlog, setSpecificBlog] = useState<Blog | null>(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getBlogById = async () => {
+      try {
+        const response = await fetch(`http://localhost:5001/blog/${id}`, {
+          method: 'GET',
+        });
+
+        if (response.ok) {
+          const responseData = await response.json();
+          setSpecificBlog(responseData);
+        } else {
+          console.error('Failed to get blog data');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getBlogById();
+  }, []);
+
+  if (!specificBlog) {
+    return <h1>Loading</h1>;
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
       <div className="bg-white p-10 rounded-lg shadow-lg">
         <BlogTitle title="Specific Blog" />
         <form className="space-y-6">
-          <BlogInputTitle />
-          <BlogInputContent />
+          <BlogTitleRead value={specificBlog.title} />
+          <BlogContentRead value={specificBlog.content} />
         </form>
         <div className="flex justify-between pt-6">
           <FormButton
