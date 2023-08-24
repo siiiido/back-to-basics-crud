47a404d4cd8d44ee3eab4243b943dfde01b29ef1
siiiido <discegaudere24.7@gmail.com>
2023-08-24 12:12:36 +0900

    test: BlogLists component

--- frontend/src/components/UI/BlogListSkeleton.tsx
+++ frontend/src/components/UI/BlogListSkeleton.tsx
@@ -1,6 +1,9 @@
 const BlogListSkeleton = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-slate-300">
+    <div
+      data-testid="blog-list-skeleton"
+      className="flex justify-center items-center h-screen bg-slate-300"
+    >
       <div className="bg-white p-10 rounded-lg shadow-lg w-1/2 h-5/6 overflow-y-scroll">
         <h1 className="text-3xl font-bold mb-8 text-center">Blog Titles</h1>
         <ul className="space-y-4">
---
+++ frontend/src/pages/BlogLists.test.tsx
@@ -0,0 +1,63 @@
+import { render, screen, waitFor } from '../utils/test-utils';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { MemoryRouter } from 'react-router-dom';
+import BlogLists from './BlogLists';
+
+const getTestData = [
+  {
+    _id: '1',
+    title: 'Test Blog 1',
+    date: '2023-08-24',
+    content: 'Test content 1',
+  },
+  {
+    _id: '2',
+    title: 'Test Blog 2',
+    date: '2023-08-23',
+    content: 'Test content 2',
+  },
+];
+
+const server = setupServer(
+  rest.get('http://localhost:5001/blog', (req, res, ctx) => {
+    return res(ctx.json(getTestData));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe('Test BlogLists Component', () => {
+  test('renders blog titles correctly', async () => {
+    render(
+      <MemoryRouter>
+        <BlogLists />
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText('Test Blog 1')).toBeInTheDocument();
+      expect(screen.getByText('Test Blog 2')).toBeInTheDocument();
+    });
+  });
+
+  test('renders skeleton when no blogs are avatilable', async () => {
+    server.use(
+      rest.get('http://localhost:5001/blog', (req, res, ctx) => {
+        return res(ctx.json([]));
+      })
+    );
+
+    render(
+      <MemoryRouter>
+        <BlogLists />
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      expect(screen.getByTestId('blog-list-skeleton')).toBeInTheDocument();
+    });
+  });
+});
